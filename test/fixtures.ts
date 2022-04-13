2dd29154ee93a5ad53784675b254b59ccc7e3e44
Melvillian <melvillevt@gmail.com>
2022-04-13 13:12:16 -0400

    uncomment fixture test

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -5,70 +5,70 @@ import { FiatTokenV2 } from "../typechain";
 
 const loadFixture = waffle.loadFixture
 
-// describe("Fixtures", function () {
-//   let usdcContractArray;
-//   beforeEach(async () => {
-//     // The slow way:
-//     usdcContractArray = await someLongSetup()
+describe("Fixtures", function () {
+  let usdcContractArray;
+  beforeEach(async () => {
+    // The slow way:
+    usdcContractArray = await someLongSetup()
 
-//     // The FAST way:
-//     //usdcContractArray = await loadFixture(usdcFixture);
-//   })
+    // The FAST way (uncomment me!):
+    // usdcContractArray = await loadFixture(usdcFixture);
+  })
 
-//   it("it", async () => {
+  it("it", async () => {
     
-//   })
+  })
 
-//   it("seems", async () => {
+  it("seems", async () => {
     
-//   })
+  })
 
-//   it("to", async () => {
+  it("to", async () => {
     
-//   })
+  })
 
-//   it("be", async () => {
+  it("be", async () => {
     
-//   })
-//   it("taking", async () => {
+  })
+  it("taking", async () => {
     
-//   })
+  })
 
-//   it("a", async () => {
+  it("a", async () => {
     
-//   })
-//   it("long", async () => {
+  })
+  it("long", async () => {
     
-//   })
-//   it("time...", async () => {
+  })
+  it("time...", async () => {
     
-//   })
-//   it("...", async () => {
+  })
+  it("...", async () => {
     
-//   })
-//   it("...", async () => {
+  })
+  it("...", async () => {
     
-//   })
-//   it("...", async () => {
+  })
+  it("...", async () => {
     
-//   })
-//   it("...", async () => {
+  })
+  it("...", async () => {
     
-//   })
+  })
 
-//   async function usdcFixture(): Promise<Array<FiatTokenV2>> {
-//     return someLongSetup();
-//   }
+  async function usdcFixture(): Promise<Array<FiatTokenV2>> {
+    return someLongSetup();
+  }
 
-//   async function someLongSetup(): Promise<Array<FiatTokenV2>> {
-//     const usdcFactory = await ethers.getContractFactory('FiatTokenV2');
+  async function someLongSetup(): Promise<Array<FiatTokenV2>> {
+    const usdcFactory = await ethers.getContractFactory('FiatTokenV2');
 
-//     const output = [];
-//     for (let i = 0; i < 10; i++) {
-//       output.push(
-//         await usdcFactory.deploy()
-//       )
-//     }
-//     return output;
-//   }
-// });
+    const output = [];
+    for (let i = 0; i < 10; i++) {
+      output.push(
+        await usdcFactory.deploy()
+      )
+    }
+    return output;
+  }
+});
